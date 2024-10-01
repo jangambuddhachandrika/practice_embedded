b263251722b441e1467d14757102771887b05979
Jangam Chandrika <jangam.chandrika@mercedes-benz.com>
2024-10-01 14:19:25 +0200

    Basic_Cpp_questions

---
+++ basic_exercises/reduce.cpp
@@ -0,0 +1,44 @@
+/************************** R E D U C E ************************
+DESCRIPTION: Reduces a positive fraction to lowest terms.
+
+PARAMETERS:
+ num, denom The numerator and denominator of the fraction to be
+ reduced. These are reference parameters, so the
+ arguments passed to them may be changed by the
+ function.
+RETURNS: 1 if the arguments are both positive, 0 otherwise.
+ALGORITHM: If both arguments are positive, then each of them is
+ divided by their greatest common divisor.
+AUTHOR: W. Knight
+*******************************************************************/
+#include<bits/stdc++.h>
+using namespace std;
+int gcd(int a,int b)
+{
+    if(a<=0||b<=0)
+    {
+        return 0;
+    }
+    while(b>0)
+    {
+        int temp = b;
+        b = b%a;
+        a = temp;
+    }
+    return a;
+}
+void reduce(int &m,int &n)
+{
+    int g = gcd(m,n);
+    m = m/g;
+    n = n/g;
+
+}
+
+int main()
+{
+    int m =10,n=25;
+    reduce(m,n);
+    cout<<m<<n<<endl;
+    return 0;
+}
